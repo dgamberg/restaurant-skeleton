f7271f5f6963129423645c7119d229f27024268d
Dave Gamberg <dgamberg@gmail.com>
2015-12-21 21:14:55 -0600

    separated angular route files

--- server/public/assets/scripts/siteRoutes.js
+++ server/public/assets/scripts/siteRoutes.js
@@ -1,104 +1,50 @@
-
 //Routes
 myApp.config(['$routeProvider', function($routeProvider){
     $routeProvider.
         when('/home', {
-            templateUrl: "/views/partials/home.html",
+            templateUrl: "/views/partials/home.html"
         }).
-
         //MENU CATEGORIES
         when('/appetizers', {
-            templateUrl: "views/menu/appetizers.html",
-            controller: "MainController"
+            templateUrl: "views/menu/appetizers.html"
         }).
         when('/burgers', {
-            templateUrl: "views/menu/burgers.html",
-            controller: "MainController"
+            templateUrl: "views/menu/burgers.html"
         }).
         when('/salads', {
-            templateUrl: "views/menu/salads.html",
-            controller: "MainController"
+            templateUrl: "views/menu/salads.html"
         }).
         when('/sandwiches', {
-            templateUrl: "views/menu/sandwiches.html",
-            controller: "MainController"
+            templateUrl: "views/menu/sandwiches.html"
         }).
         when('/kids-menu', {
-            templateUrl: "views/menu/kids.html",
-            controller: "MainController"
+            templateUrl: "views/menu/kids.html"
         }).
 
         // SITE LINKS
         when('/shopping-cart', {
-            templateUrl: "views/partials/shoppingCart.html",
-            controller: "MainController"
+            templateUrl: "views/partials/shoppingCart.html"
         }).
         when('/about', {
-            templateUrl: "views/partials/about.html",
-            controller: "MainController"
+            templateUrl: "views/partials/about.html"
         }).
         when('/contact', {
-            templateUrl: "views/partials/contact.html",
-            controller: "MainController"
+            templateUrl: "views/partials/contact.html"
         }).
         when('/directions', {
-            templateUrl: "views/partials/directions.html",
-            controller: "MainController"
+            templateUrl: "views/partials/directions.html"
         }).
 
         //USER LINKS
         when('/login', {
-            templateUrl: "views/customer/login.html",
-            controller: "MainController"
+            templateUrl: "views/customer/login.html"
         }).
         when('/register', {
-            templateUrl: "views/customer/register.html",
-            controller: "MainController"
+            templateUrl: "views/customer/register.html"
         }).
 
         //USER LOGIN
         when('/user', {
-            templateUrl: "views/admin/user.html",
-            controller: "MainController"
-        }).
-
-        //ADMIN LINKS
-        when('/admin-login', {
-            templateUrl: "views/admin/admin-login.html",
-            controller: "MainController"
-        }).
-        when('/admin', {
-            templateUrl: "views/admin/admin.html",
-            controller: "MainController"
-        }).
-        // DASHBOARD MENU
-        when('/admin-list-menuItems', {
-            templateUrl: "views/admin/admin-list-menuItems.html",
-            controller: "MainController"
-        }).
-        when('/admin-add-menuItem', {
-            templateUrl: "views/admin/admin-add-menuItem.html",
-            controller: "MainController"
-        }).
-        //CUSTOMERS
-        when('/admin-add-customer', {
-            templateUrl: "views/admin/admin-add-customer.html",
-            controller: "MainController"
-        }).
-
-        when('/admin-list-customers', {
-            templateUrl: "views/admin/admin-list-customers.html",
-            controller: "MainController"
-        }).
-        when('/admin-live-orders', {
-            templateUrl: "views/admin/admin-live-orders.html",
-            controller: "MainController"
-        }).
-        when('/failure', {
-            templateUrl: "views/customer/failure.html",
-            controller: "MainController"
-        }).
-        otherwise({
-            redirectTo: '/home'
-        })
+            templateUrl: "views/admin/user.html"
+        });
 }]);
